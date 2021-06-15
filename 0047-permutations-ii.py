47248baecca439624b7e67afa6b0985b0742569e
Thuan Tang <thuantang@gmail.com>
2021-06-15 10:41:05 -0700

    Faster permutation-ii

--- 0047-permutations-ii.py
+++ 0047-permutations-ii.py
@@ -13,14 +13,54 @@ class Solution:
         output = []
 
         if nums:
-            visited = set()
+            nums = sorted(nums)
+            visited = [False] * len(nums)
             permutation = []
             self.dfs(nums, visited, permutation, output)
 
         return output
 
-    def dfs(self, nums: List[int], visited, permutation, output):
-        """Preorder traversal"""
+    def dfs(self, nums, visited, permutation, output):
+        """
+        Preorder traversal
+        Not bruteforce to check for unique permutation
+        """
+        # base case
+        # done with permutation when length is same as length of nums
+        if len(nums) == len(permutation):
+            output.append(list(permutation))
+        else:
+            for i in range(len(nums)):
+                if visited[i]:
+                    continue
+                if i > 0 and nums[i] == nums[i-1] and not visited[i-1]:
+                    continue
+
+                # visit node
+                permutation.append(nums[i])
+                visited[i] = True
+
+                self.dfs(nums, visited, permutation, output)
+
+                # undo so we can permute others
+                visited[i] = False
+                permutation.pop()
+
+    def permuteUnique2(self, nums: List[int]) -> List[List[int]]:
+        output = []
+
+        if nums:
+            visited = set()
+            permutation = []
+            self.dfs2(nums, visited, permutation, output)
+
+        return output
+
+    def dfs2(self, nums: List[int], visited, permutation, output):
+        """
+        Preorder traversal
+        Bruteforce-ish to get for unique permutation
+        """
         # base case
         # done with permutation when length is same as length of nums
         if len(nums) == len(permutation):
@@ -37,7 +77,7 @@ class Solution:
                     visited.add(i)
 
                     # visit children
-                    self.dfs(nums, visited, permutation, output)
+                    self.dfs2(nums, visited, permutation, output)
 
                     # undo so we can permute others
                     visited.remove(i)
@@ -45,12 +85,17 @@ class Solution:
 
 
 if __name__ == "__main__":
-    nums = [1, 1, 2]
-    solution = Solution()
-    answer = solution.permuteUnique(nums)
-    print(answer)
+    # nums = [2, 1, 1]
+    # solution = Solution()
+    # answer = solution.permuteUnique(nums)
+    # print(answer)
 
-    nums = [1, 2, 3]
+    # nums = [1, 2, 3]
+    # solution = Solution()
+    # answer = solution.permuteUnique(nums)
+    # print(answer)
+
+    nums = [3, 3, 0, 3]
     solution = Solution()
     answer = solution.permuteUnique(nums)
     print(answer)
