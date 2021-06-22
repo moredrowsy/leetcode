06c230bd4effaedf0be1f960136ee4dae71b1afc
Thuan Tang <thuantang@gmail.com>
2021-06-22 11:53:35 -0700

    Improved triangle solution with no extra space complexity

--- 0120-triangle.py
+++ 0120-triangle.py
@@ -5,7 +5,7 @@ https://leetcode.com/problems/triangle/
 
 Given a triangle array, return the minimum path sum from top to bottom.
 
-For each step, you may move to an adjacent number of the row below. More formally, if you are on index i on the current row, you may move to either index i or index i + 1 on the next row.
+For each step, you may move to an adjacent number of the rows below. More formally, if you are on index i on the current rows, you may move to either index i or index i + 1 on the next rows.
 """
 from typing import List
 
@@ -13,28 +13,24 @@ from typing import List
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         if triangle:
-            row = len(triangle)
-            dp = [[0] * len(triangle[i]) for i in range(row)]
+            rows = len(triangle)
 
-            # init
-            dp[0][0] = triangle[0][0]
+            for i in range(1, rows):
+                cols = len(triangle[i])
 
-            for i in range(1, row):
-                col = len(triangle[i])
-
-                for j in range(col):
+                for j in range(cols):
                     # left edge
                     if j == 0:
-                        dp[i][j] = triangle[i][j] + dp[i-1][j]
+                        triangle[i][j] += triangle[i-1][j]
                     # right edge
-                    elif j == len(triangle[i]) - 1:
-                        dp[i][j] = triangle[i][j] + dp[i-1][j-1]
+                    elif j == cols - 1:
+                        triangle[i][j] += triangle[i-1][j-1]
                     else:
-                        dp[i][j] = triangle[i][j] + min(
-                            dp[i-1][j], dp[i-1][j-1]
+                        triangle[i][j] += min(
+                            triangle[i-1][j], triangle[i-1][j-1]
                         )
 
-            return min(dp[row-1])
+            return min(triangle[rows-1])
         else:
             return 0
 
