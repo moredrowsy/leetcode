bbfe96bebf865b7549b141a0ad4f515e34332482
Thuan Tang <thuantang@gmail.com>
2021-08-21 22:20:14 -0700

    Added 0011 and sort matrix by spiral

---
+++ 0011-search-range-in-binary-tree.py
@@ -0,0 +1,70 @@
+"""
+11. Search Range in Binary Search Tree
+Medium
+https://www.lintcode.com/problem/11/
+
+Given a binary search tree and a range [k1, k2], return node values within a given range in ascending order.
+"""
+
+from tree_node import TreeNode
+
+
+class Solution:
+    """
+    @param root: param root: The root of the binary search tree
+    @param k1: An integer
+    @param k2: An integer
+    @return: return: Return all keys that k1<=key<=k2 in ascending order
+    """
+
+    def searchRange(self, root, k1, k2):
+        output = []
+        self.bst(root, k1, k2, output)
+
+        return output
+
+    def bst(self, root, k1, k2, output):
+        if root:
+            if root.val > k1:
+                self.bst(root.left, k1, k2, output)
+
+            if k1 <= root.val <= k2:
+                output.append(root.val)
+
+            if root.val < k2:
+                self.bst(root.right, k1, k2, output)
+
+
+if __name__ == "__main__":
+    nodes = [20, 8, 22, 4, 12]
+    k1 = 6
+    k2 = 10
+    nodes = [TreeNode(node) if node else None for node in nodes]
+    tree = TreeNode.get_tree_from_treenode_list(nodes)
+    output = Solution().searchRange(tree, k1, k2)
+    expected = [8]
+    print(f"\noutput\t\t{output}")
+    print(f"expected\t{expected}")
+    print(output == expected)
+
+    nodes = [31, 11, 51, 8,  None, 41, 61]
+    k1 = 9
+    k2 = 52
+    nodes = [TreeNode(node) if node else None for node in nodes]
+    tree = TreeNode.get_tree_from_treenode_list(nodes)
+    output = Solution().searchRange(tree, k1, k2)
+    expected = [11, 31, 41, 51]
+    print(f"\noutput\t\t{output}")
+    print(f"expected\t{expected}")
+    print(output == expected)
+
+    nodes = [20, 1, 40,  None, None, 35]
+    k1 = 17
+    k2 = 37
+    nodes = [TreeNode(node) if node else None for node in nodes]
+    tree = TreeNode.get_tree_from_treenode_list(nodes)
+    output = Solution().searchRange(tree, k1, k2)
+    expected = [20, 35]
+    print(f"\noutput\t\t{output}")
+    print(f"expected\t{expected}")
+    print(output == expected)
---
+++ sort-matrix-from-outside-to-inside.py
@@ -0,0 +1,113 @@
+"""
+Sort matrix outside and then inside
+"""
+
+
+class Solution:
+    def sort_matrix(self, matrix):
+        """
+        Sort outside and then sort inside
+        """
+        m, n = len(matrix), len(matrix[0])
+
+        top, bottom = 0, m-1
+        left, right = 0, n-1
+
+        while top <= bottom and left <= right:
+            linear = self.spiral_to_linear(matrix, top, bottom, left, right)
+            linear.sort()
+            self.linear_to_spiral(linear, matrix, top, bottom, left, right)
+
+            top += 1
+            bottom -= 1
+            left += 1
+            right -= 1
+
+        return matrix
+
+    def spiral_to_linear(self, matrix, top, bottom, left, right):
+        linear = []
+
+        # top
+        for i in range(left, right+1):
+            linear.append(matrix[top][i])
+
+        # right
+        for i in range(top+1, bottom):
+            linear.append(matrix[i][right])
+
+        # bottom
+        if top != bottom:
+            for i in range(right, left-1, -1):
+                linear.append(matrix[bottom][i])
+
+        # left
+        if left != right:
+            for i in range(bottom-1, top, -1):
+                linear.append(matrix[i][left])
+
+        return linear
+
+    def linear_to_spiral(self, linear, matrix, top, bottom, left, right):
+        n = 0
+
+        # top
+        for i in range(left, right+1):
+            matrix[top][i] = linear[n]
+            n += 1
+
+        # right
+        for i in range(top+1, bottom):
+            matrix[i][right] = linear[n]
+            n += 1
+
+        # bottom
+        if top != bottom:
+            for i in range(right, left-1, -1):
+                matrix[bottom][i] = linear[n]
+                n += 1
+
+        # left
+        if left != right:
+            for i in range(bottom-1, top, -1):
+                matrix[i][left] = linear[n]
+                n += 1
+
+
+if __name__ == "__main__":
+    t = [2, 100, 6, 4, 5, 6, 5, 9, 10, -1, 3, 5, 7, 3]
+    t.sort()
+    print(t)
+    u = [4, 5, 3, 1]
+    u.sort()
+    print(u)
+
+    matrix = [
+        [2, 4, 5, 6],
+        [3, 3, 1, 5],
+        [9, 3, 5, 7]
+    ]
+    output = Solution().sort_matrix(matrix)
+    expected = [
+        [2, 3, 3, 4],
+        [9, 1, 3, 5],
+        [7, 6, 5, 5]
+    ]
+    print(f"\noutput\t\t{output}")
+    print(f"expected\t{expected}")
+    print(output == expected)
+
+    matrix = [
+        [2, 100, 6, 4, 5, 6],
+        [3, 4, 5, 3, 1, 5],
+        [9, 10, -1, 3, 5, 7]
+    ]
+    output = Solution().sort_matrix(matrix)
+    expected = [
+        [-1, 2, 3, 3, 4, 5],
+        [100, 1, 3, 4, 5, 5],
+        [10, 9, 7, 6, 6, 5]
+    ]
+    print(f"\noutput\t\t{output}")
+    print(f"expected\t{expected}")
+    print(output == expected)
