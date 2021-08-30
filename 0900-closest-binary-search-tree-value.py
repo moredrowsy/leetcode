5fe1ff8abee410e93d905d147485aed70edf8573
Thuan Tang <thuantang@gmail.com>
2021-08-29 20:39:39 -0700

    Updated 0900

--- 0900-closest-binary-search-tree-value.py
+++ 0900-closest-binary-search-tree-value.py
@@ -22,10 +22,6 @@ class TreeNode:
 
 
 class Solution:
-    def __init__(self) -> None:
-        self.closest_node = None
-        self.min_dist = float('inf')
-
     def closestValue(self, root, target):
         """
         @param root: the given BST
@@ -40,27 +36,62 @@ class Solution:
         ----------------
         O(n)
         """
-        self.dfs(root, target)
-        return self.closest_node.val
+        return self.dfs_re(root, target)
 
-    def dfs(self, root, target):
+    def dfs_iter(self, root, target):
+        upper = root
+        lower = root
+        while root:
+            if target > root.val:
+                lower = root
+                root = root.right
+            elif target < root.val:
+                upper = root
+                root = root.left
+            else:
+                return root.val
+        if abs(upper.val - target) <= abs(lower.val - target):
+            return upper.val
+        return lower.val
+
+    def dfs_re(self, root, target):
+        lower = self.lower_bound(root, target)
+        upper = self.upper_bound(root, target)
+
+        if not lower:
+            return upper.val
+        if not upper:
+            return lower.val
+        if abs(lower.val - target) < abs(upper.val - target):
+            return lower.val
+        return upper.val
+
+    def lower_bound(self, root, target):
         if not root:
-            return
+            return None
 
-        dist = abs(root.val - target)
-        if dist < self.min_dist:
-            self.closest_node = root
-            self.min_dist = dist
+        if target < root.val:
+            return self.lower_bound(root.left, target)
 
-        self.dfs(root.left, target)
-        self.dfs(root.right, target)
+        lower = self.lower_bound(root.right, target)
+        return lower if lower else root
+
+    def upper_bound(self, root, target):
+        if not root:
+            return None
+
+        if target >= root.val:
+            return self.upper_bound(root.right, target)
+
+        upper = self.upper_bound(root.left, target)
+        return upper if upper else root
 
 
 if __name__ == "__main__":
     nodes = [5, 4, 9, 2, None, 8, 10]
+    target = 6.124780
     nodes = [TreeNode(node) if node else None for node in nodes]
     tree = TreeNode.get_tree_from_treenode_list(nodes)
-    target = 6.124780
     output = Solution().closestValue(tree, target)
     expected = 5
     print(f"\noutput\t\t{output}")
@@ -68,9 +99,19 @@ if __name__ == "__main__":
     print(output == expected)
 
     nodes = [3, 2, 4, 1]
+    target = 4.142857
     nodes = [TreeNode(node) if node else None for node in nodes]
     tree = TreeNode.get_tree_from_treenode_list(nodes)
+    output = Solution().closestValue(tree, target)
+    expected = 4
+    print(f"\noutput\t\t{output}")
+    print(f"expected\t{expected}")
+    print(output == expected)
+
+    nodes = [10, 5, 15, 3, 6, 12, 18, None, 4, None, 8]
     target = 4.142857
+    nodes = [TreeNode(node) if node else None for node in nodes]
+    tree = TreeNode.get_tree_from_treenode_list(nodes)
     output = Solution().closestValue(tree, target)
     expected = 4
     print(f"\noutput\t\t{output}")
