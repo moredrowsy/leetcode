d9d793125dc76614cb6aff3484e6d3220f678a7c
Thuan Tang <thuantang@gmail.com>
2022-06-14 19:57:34 -0500

    Added 0009-palindrome-number

---
+++ 0009-palindrome-number.py
@@ -0,0 +1,59 @@
+"""
+9. Palindrome Number
+Easy
+https://leetcode.com/problems/palindrome-number/
+
+Given an integer x, return true if x is palindrome integer.
+
+An integer is a palindrome when it reads the same backward as forward.
+
+For example, 121 is a palindrome while 123 is not.
+"""
+
+
+class Solution:
+    """
+    Time Complexity
+    ---------------
+    O(n)
+
+    Space Complexity
+    ----------------
+    O(1)
+    """
+
+    def isPalindrome(self, x: int) -> bool:
+        num = str(x)
+        left, right = 0, len(num) - 1
+
+        while left < right:
+            if num[left] != num[right]:
+                return False
+
+            left += 1
+            right -= 1
+
+        return True
+
+
+if __name__ == "__main__":
+    x = 121
+    output = Solution().isPalindrome(x)
+    expected = True
+    print(f"\noutput\t\t{output}")
+    print(f"expected\t{expected}")
+    print(output == expected)
+
+    x = -121
+    output = Solution().isPalindrome(x)
+    expected = False
+    print(f"\noutput\t\t{output}")
+    print(f"expected\t{expected}")
+    print(output == expected)
+
+    x = 10
+    output = Solution().isPalindrome(x)
+    expected = False
+    print(f"\noutput\t\t{output}")
+    print(f"expected\t{expected}")
+    print(output == expected)
